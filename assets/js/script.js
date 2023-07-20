ddd543709cff188c36c6769efca355fd79eef262
id10tothe9 <id10tothe9@googlemail.com>
2023-07-20 19:10:37 +0000

    feat(JS): Add player control of right paddle
    Via eventListeners to mouse and touch movements

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,43 +12,69 @@
 /** 1. Initiation ******************************************************************************* 
 *   Initiate objects and eventListeners                                                         *
 */
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
     // Get elements of of the gameboard as an array of objects:
     // [gameArea,ball,paddleLeft,paddleRight,ballElement,paddleLeftElement,paddleRightElement]
     let gameObjects = getGameObjects(); // Index: Assistant functions
-  
-  });
-  /** Initiation - End ***************************************** */
-  
+
+    // Let user control the right paddle with mouse pointer and touchscreen
+    document.addEventListener('mousemove', function (event) {
+        movePlayerPaddle(event, gameObjects, ...gameObjects);
+    });
+    document.addEventListener('touchmove', function (event) {
+        movePlayerPaddle(event.changedTouches[0], gameObjects, ...gameObjects);
+    });
+
+});
+/** Initiation - End ***************************************** */
+
+
+
+function movePlayerPaddle(event, gameObjects, gameArea, ball, paddleLeft, paddleRight, ballElement, paddleLeftElement, paddleRightElement) {
+    // Define position inside gameboard and move paddle only within it
+    let pointerInGameArea = event.clientY - gameArea.topY;
+    let pointerTopBoundary = paddleRight.height / 2;
+    let pointerBottomBoundary = gameArea.height - paddleRight.height / 2;
+    if (pointerInGameArea >= pointerTopBoundary && pointerInGameArea <= pointerBottomBoundary) {
+        let deltaY = pointerInGameArea - paddleRight.height / 2;
+        paddleRightElement.style.top = `${deltaY}px`;
+    } else if (pointerInGameArea < pointerTopBoundary) {
+        paddleRightElement.style.top = '0px';
+    } else if (pointerInGameArea > pointerBottomBoundary) {
+        let deltaY = gameArea.height - paddleRight.height;
+        paddleRightElement.style.top = `${deltaY}px`;
+    }
+}
+
 
 /** Assistant Functions *************************************************
 *    Functions related to getting and setting dimensions and positions  *
 *    of elements on the gameboard.                                      *
-*/ 
+*/
 function getGameObjects() {
     //Get dimensions of the elements in the game
-      let gameAreaElement = document.getElementById('game-area');
-      let ballElement = document.getElementById('ball');
-      let paddleLeftElement = document.getElementById('paddle-left');
-      let paddleRightElement = document.getElementById('paddle-right');
-      // Defined as objects with width and height in px
-      let gameArea = getDimensions(gameAreaElement);
-      let ball = getDimensions(ballElement);
-      let paddleLeft = getDimensions(paddleLeftElement);
-      let paddleRight = getDimensions(paddleRightElement);
+    let gameAreaElement = document.getElementById('game-area');
+    let ballElement = document.getElementById('ball');
+    let paddleLeftElement = document.getElementById('paddle-left');
+    let paddleRightElement = document.getElementById('paddle-right');
+    // Defined as objects with width and height in px
+    let gameArea = getDimensions(gameAreaElement);
+    let ball = getDimensions(ballElement);
+    let paddleLeft = getDimensions(paddleLeftElement);
+    let paddleRight = getDimensions(paddleRightElement);
     //-done-
-    
-      // get absolute Y coordinate of gameArea to calculate relative pointer position in movePlayerPaddle
-      gameArea.topY = gameAreaElement.getBoundingClientRect().top;
-      
-      return [gameArea, ball, paddleLeft, paddleRight, ballElement, paddleLeftElement, paddleRightElement];
-    }
-    
-    // Get width and height of each div element
-    function getDimensions(divElement) {
-      let divStyle = getComputedStyle(divElement);
-      let div = {};
-      div.width = parseFloat(divStyle.width);
-      div.height = parseFloat(divStyle.height);
-      return div;
-    }
+
+    // get absolute Y coordinate of gameArea to calculate relative pointer position in movePlayerPaddle
+    gameArea.topY = gameAreaElement.getBoundingClientRect().top;
+
+    return [gameArea, ball, paddleLeft, paddleRight, ballElement, paddleLeftElement, paddleRightElement];
+}
+
+// Get width and height of each div element
+function getDimensions(divElement) {
+    let divStyle = getComputedStyle(divElement);
+    let div = {};
+    div.width = parseFloat(divStyle.width);
+    div.height = parseFloat(divStyle.height);
+    return div;
+}
