954e8530105a9da7606321f2bff8d859fb109fd7
id10tothe9 <id10tothe9@googlemail.com>
2023-07-23 04:18:54 +0000

    fix: Move element id to bullet instead of div
    This enables us to target the bullet element
    directly to change its class to solid or regular.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 window.addEventListener('DOMContentLoaded', function () {
-
+  // Get main buttons
   let storyBtn = document.getElementById('btn-story-time');
   let howtoBtn = document.getElementById('btn-howto');
+  // Get intro-boxes and their styles
   let storyBox = document.getElementById('story-box');
   let storyBoxStyle = window.getComputedStyle(storyBox);
   let howtoBox = document.getElementById('howto-box');
   let howtoBoxStyle = window.getComputedStyle(howtoBox);
 
-
-  storyBtn.addEventListener('click', function(event) {
+  storyBtn.addEventListener('click', function (event) {
     if (!this.classList.contains('active')) { // credit: property .contains and condition negation from ChatGPT4
       // Credit: idea to change class of active button and how to do it from ChatGPT4
       this.classList.add('active');
@@ -19,12 +19,9 @@ window.addEventListener('DOMContentLoaded', function () {
         howtoBox.style.display = 'none';
       }
     }
-});
+  });
 
-
-
-
-  howtoBtn.addEventListener('click', function(event) {
+  howtoBtn.addEventListener('click', function (event) {
     if (!this.classList.contains('active')) { // credit: property .contains and condition negation from ChatGPT4
       // Credit: idea to change class of active button and how to do it from ChatGPT4
       this.classList.add('active');
@@ -35,6 +32,22 @@ window.addEventListener('DOMContentLoaded', function () {
         storyBox.style.display = 'none';
       }
     }
+  });
+
+  // Get navigation elements
+  let storyArrowLeft = document.getElementById('story-arrow-left');
+  let storyBullet1 = document.getElementById('story-arrow-left');
+  let storyBullet2 = document.getElementById('story-arrow-left');
+  let storyBullet3 = document.getElementById('story-arrow-left');
+  let storyArrowRight = document.getElementById('story-arrow-left');
+  let howtoArrowLeft = document.getElementById('story-arrow-left');
+  let howtoBullet1 = document.getElementById('story-arrow-left');
+  let howtoBullet2 = document.getElementById('story-arrow-left');
+  let howtoBullet3 = document.getElementById('story-arrow-left');
+  let howtoArrowRight = document.getElementById('story-arrow-left');
+
+  storyArrowLeft.addEventListener('click', function(event) {
+    
   });
 
 });
