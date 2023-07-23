1e1f3b4f4fc8a4fa448c9d85b82f057c33ac07b8
id10tothe9 <id10tothe9@googlemail.com>
2023-07-23 03:24:41 +0000

    feat: Add eventlisteners to buttons on homepage

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+  let storyBtn = document.getElementById('btn-story-time');
+
+  storyBtn.addEventListener('click', function(event) {
+
+  });
+
+
+
+  let howtoBtn = document.getElementById('btn-howto');
+
+  howtoBtn.addEventListener('click', function(event) {
+    
+  });
+
+});
