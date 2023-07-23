61ebf87216e01543e81a20704ebcd4cafa3055cb
id10tothe9 <id10tothe9@googlemail.com>
2023-07-23 03:44:40 +0000

    feat: Change active state of buttons with click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,26 @@
 window.addEventListener('DOMContentLoaded', function () {
 
   let storyBtn = document.getElementById('btn-story-time');
-
-  storyBtn.addEventListener('click', function(event) {
-
-  });
-
-
-
   let howtoBtn = document.getElementById('btn-howto');
 
+
+  storyBtn.addEventListener('click', function(event) {
+    if (!this.classList.contains('active')) { // credit: property .contains and condition negation from ChatGPT4
+      // Credit: idea to change class of active button and how to do it from ChatGPT4
+      this.classList.add('active');
+      howtoBtn.classList.remove('active');
+    }
+});
+
+
+
+
   howtoBtn.addEventListener('click', function(event) {
-    
+    if (!this.classList.contains('active')) { // credit: property .contains and condition negation from ChatGPT4
+      // Credit: idea to change class of active button and how to do it from ChatGPT4
+      this.classList.add('active');
+      storyBtn.classList.remove('active');
+    }
   });
 
 });
